2fa7f4c98cd4ea29766fd9b63980d39e2927c48e
Anass Lahrech <anaslahrech@gmail.com>
2019-04-11 19:16:56 +0200

    Delete the RT at the beginning of tweets, convert emojis to text and add file of Emoji Sentiment Data for analyse sentiment purposes

--- only_text_tweet.py
+++ only_text_tweet.py
@@ -1,11 +1,8 @@
 import json
 import os
+import re
+import emoji
 
-pathdownload="crawler/downloads/"
-filename = "1553850539719.json"
-
-newfilename="extracted"+filename[:-5]+".txt"
-pathnewfile="tweet-texts/"
 
 def extract_text(pathdownload,filename,pathnewfile,newfilename):
     newf = open(pathnewfile + newfilename, 'w')
@@ -14,20 +11,33 @@ def extract_text(pathdownload,filename,pathnewfile,newfilename):
         distros_dict = json.load(f)
 
     for distro in distros_dict["statuses"]:
-        print(distro["text"])
-        print("\n \n \n \n \n \n")
+        print(len(distro["text"]))
+
         if (os.stat(pathnewfile + newfilename).st_size == 0):
-            newf.write(distro["text"])
+
+            tweet = distro["text"]
+            tweet = re.sub("RT", "", tweet)
+            tweet = re.sub("\n","", tweet)
+            tweet = emoji.demojize(tweet) #Convert emojis to text for sentiment analysis purposes
+            newf.write(tweet)
             newf.write("\n \n")
 
     newf.close()
 
+pathdownload="crawler/downloads/"
+pathnewfile="tweet-texts/"
 
-#
+
 # for root, dirs, files in os.walk(pathdownload):
 #     for filename in files:
 #         print(filename)
+#         newfilename = filename[:-5] + ".txt"
+#         extract_text(pathdownload,filename,pathnewfile,newfilename)
 
+
+filename = "1553850539719.json"
+
+newfilename=filename[:-5]+".txt"
 
 extract_text(pathdownload,filename,pathnewfile,newfilename)
 
